028ef53f0e1f9d6b9294afad61a1b64c2d13944d
Alaamhmdgalal <alaaemran99@gmail.com>
2022-07-08 23:28:53 +0200

    Changes

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -11,70 +11,98 @@ class LoginScreen extends StatelessWidget{
       appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [
-            const Text(
-              'Login',
-              style: TextStyle(
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(
-              height: 40.0,
-            ),
-            TextFormField(
-              controller: emailController = TextEditingController(),
-              keyboardType: TextInputType.emailAddress,
-              onFieldSubmitted: (String value){
-                print(value);
-              },
-              onChanged: (String value){
-                print(value);
-              },
-              decoration: const InputDecoration(
-                labelText: 'Email Address',
-                prefixIcon: Icon(
-                  Icons.email,
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Login',
+                  style: TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-                border: OutlineInputBorder(),
-              ),
-            ),
-            const SizedBox(
-              height: 15.0,
-            ),
-            const Text(
-              'Password',
-              style: TextStyle(
-                fontSize: 40.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(
-              height: 40.0,
-            ),
-            TextFormField(
-              controller: passwordController,
-              keyboardType: TextInputType.visiblePassword,
-              obscureText: true,
-              onFieldSubmitted: (String value){
-                print(value);
-              },
-              decoration: const InputDecoration(
-                labelText: 'Password',
-                prefixIcon: Icon(
-                  Icons.lock,
+                const SizedBox(
+                  height: 40.0,
                 ),
-                suffixIcon: Icon(
-                  Icons.remove_red_eye,
+                TextFormField(
+                  controller: emailController = TextEditingController(),
+                  keyboardType: TextInputType.emailAddress,
+                  onFieldSubmitted: (value){
+                    print(value);
+                  },
+                  onChanged: (String value){
+                    print(value);
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Email Address',
+                    prefixIcon: Icon(
+                      Icons.email,
+                    ),
+                    border: OutlineInputBorder(),
+                  ),
                 ),
-                border: OutlineInputBorder(),
-              ),
+                const SizedBox(
+                  height: 15.0,
+                ),
+                TextFormField(
+                  controller: passwordController,
+                  keyboardType: TextInputType.visiblePassword,
+                  obscureText: true,
+                  onFieldSubmitted: (String value){
+                    print(value);
+                  },
+                  decoration: const InputDecoration(
+                    labelText: 'Password',
+                    prefixIcon: Icon(
+                      Icons.lock,
+                    ),
+                    suffixIcon: Icon(
+                      Icons.remove_red_eye,
+                    ),
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  width: double.infinity,
+                  color: Colors.blue,
+                  child: MaterialButton(
+                      onPressed: (){
+                        print(emailController.text);
+                        print(passwordController.text);
+                      },
+                  child: const Text(
+                    'LOGIN',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Don\'t have an account?',
+                    ),
+                    TextButton(onPressed: (){
+
+                    },
+                        child: Text(
+                          'Register now',
+                        ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-            const SizedBox(
-              height: 20.0,
-            ),
-          ],
+          ),
         ),
       ),
     );
