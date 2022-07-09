bc36974728e6d719336118e5d9b9ac6a5a924942
Alaamhmdgalal <alaaemran99@gmail.com>
2022-07-09 04:11:58 +0200

    Counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:udemy_project/counter_screen.dart';
 import 'package:udemy_project/home_screen.dart';
 import 'package:udemy_project/login_screen.dart';
 import 'package:udemy_project/messenger_screen.dart';
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget{
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: UsersScreen(),
+      home: CounterScreen(),
     );
   }
   
