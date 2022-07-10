3b792e641ac7d8fc87e95e867e767d3786c8c5c5
Alaamhmdgalal <alaaemran99@gmail.com>
2022-07-10 02:06:06 +0200

    Clean Code

--- lib/modules/bmi/bmi_screen.dart
+++ lib/modules/bmi/bmi_screen.dart
@@ -1,7 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
-import 'package:udemy_project/bmi_result_screen.dart';
+import 'package:udemy_project/modules/bmi_result/bmi_result_screen.dart';
 
 class BmiScreen extends StatefulWidget
 {
--- lib/modules/users/users_screen.dart
+++ lib/modules/users/users_screen.dart
@@ -1,17 +1,5 @@
 import 'package:flutter/material.dart';
-
-class UserModel
-{
-  final int id;
-  final String name;
-  final String phone;
-
-  UserModel({
-    required this.id,
-    required this.name,
-    required this.phone
-  });
-}
+import 'package:udemy_project/models/user/user_model.dart';
 
 class UsersScreen extends StatelessWidget{
   List<UserModel> users = [
